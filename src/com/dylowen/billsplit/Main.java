d739483bdb3bb3ff1cdace07479950d8732101e1
Dylan Owen <dyltotheo@gmail.com>
2016-01-27 11:37:33 -0800

    First commit

---
+++ src/com/dylowen/billsplit/Main.java
@@ -0,0 +1,15 @@
+package com.dylowen.billsplit;
+
+/**
+ * TODO add description
+ *
+ * @author dylan.owen
+ * @since Jan-2016
+ */
+public class Main {
+
+    public static void main(String [] args) {
+        System.out.println("Hello");
+    }
+
+}
